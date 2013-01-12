828c3e01a98b52b59d669cd671381bb281043580
Clemens Buchacher <drizzd@aon.at>
2013-01-12 19:29:55 +0100

    chart

--- src/at/drizzd/Andenv/Andenv.java
+++ src/at/drizzd/Andenv/Andenv.java
@@ -2,14 +2,56 @@ package at.drizzd.Andenv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class Andenv extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+import org.achartengine.GraphicalView;
+import org.achartengine.ChartFactory;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+public class Andenv extends Activity {
+    private GraphicalView mChart;
+
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+
+    private XYSeries mCurrentSeries;
+
+    private XYSeriesRenderer mCurrentRenderer;
+
+    private void initChart() {
+        mCurrentSeries = new XYSeries("Sample Data");
+        mDataset.addSeries(mCurrentSeries);
+        mCurrentRenderer = new XYSeriesRenderer();
+        mRenderer.addSeriesRenderer(mCurrentRenderer);
+    }
+
+    private void addSampleData() {
+        mCurrentSeries.add(1, 2);
+        mCurrentSeries.add(2, 3);
+        mCurrentSeries.add(3, 2);
+        mCurrentSeries.add(4, 5);
+        mCurrentSeries.add(5, 4);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+        if (mChart == null) {
+            initChart();
+            addSampleData();
+            mChart = ChartFactory.getCubeLineChartView(this, mDataset, mRenderer, 0.3f);
+            layout.addView(mChart);
+        } else {
+            mChart.repaint();
+        }
     }
 }
