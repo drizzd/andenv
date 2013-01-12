7a7b10cf388ca6adf282cbe58fc9d170d0b3896f
Clemens Buchacher <drizzd@aon.at>
2013-01-12 16:37:04 +0100

    update

--- src/at/drizzd/Andenv/Andenv.java
+++ src/at/drizzd/Andenv/Andenv.java
@@ -1,9 +1,11 @@
 package at.drizzd.Andenv;
 
+import java.lang.Short;
 import java.lang.Math;
 import java.lang.Thread;
 import java.util.Arrays;
 import java.lang.InterruptedException;
+import java.lang.String;
 
 import android.util.Log;
 import android.app.Activity;
@@ -11,17 +13,25 @@ import android.os.Bundle;
 import android.media.AudioFormat;
 import android.media.MediaRecorder;
 import android.media.AudioRecord;
+import android.widget.TextView;
 
 public class Andenv extends Activity
 {
+    /* Averaging period in seconds. */
+    double avgPeriod = 1.0;
+    /* Update rate in multiples per second. */
+    final int updateRate = 2;
+
     private static String TAG = "Andenv";
+    TextView view = null;
     final int audioSource = MediaRecorder.AudioSource.MIC;
     final int sampleRate = 44100;
     final int channelConfig = AudioFormat.CHANNEL_IN_MONO;
-    final int audioFormat = AudioFormat.ENCODING_PCM_8BIT;
-    final int bufferSizeInBytes = AudioRecord.getMinBufferSize(
-            sampleRate, channelConfig, audioFormat);
-    Thread runner;
+    final int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    final int bytesPerSample = 2;
+    final int readBufferLen = sampleRate/updateRate;
+    int bufferSizeInBytes;
+    Thread mConsumer;
     AudioRecord mAudioRecord;
     volatile boolean mActive = true;
 
@@ -31,43 +41,89 @@ public class Andenv extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        if (bufferSizeInBytes < 0) {
-            Log.e(TAG, "getMinBufferSize error: " + bufferSizeInBytes);
-        }
-        mAudioRecord = new AudioRecord(
-                audioSource,
-                sampleRate,
-                channelConfig,
-                audioFormat,
-                bufferSizeInBytes);
+        view = (TextView)findViewById(R.id.body);
 
-        mAudioRecord.startRecording();
-        new Thread(new Runnable() {
+        bufferSizeInBytes = AudioRecord.getMinBufferSize(
+            sampleRate, channelConfig, audioFormat);
+        if (bufferSizeInBytes < 0) {
+            Log.e(TAG, "AudioRecord.getMinBufferSize: " + bufferSizeInBytes);
+            return;
+        }
+        Log.i(TAG, "minimum buffer size: " + bufferSizeInBytes);
+        bufferSizeInBytes = Math.max(2 * readBufferLen * bytesPerSample, bufferSizeInBytes);
+        Log.i(TAG, "selected buffer size: " + bufferSizeInBytes);
+
+        startRecording();
+    }
+
+    public void startRecording() {
+        try {
+            mAudioRecord = new AudioRecord(
+                    audioSource,
+                    sampleRate,
+                    channelConfig,
+                    audioFormat,
+                    bufferSizeInBytes);
+
+            mAudioRecord.startRecording();
+        } catch (Throwable t) {
+            Log.e(TAG, "AudioRecord", t);
+            return;
+        }
+
+        mConsumer = new Thread(new Runnable() {
             public void run() {
-                byte[] audioData = new byte[bufferSizeInBytes];
+                double avgLen = sampleRate * avgPeriod;
+                double alpha = 2/(avgLen-1);
+                double power = 0;
+                short[] readBuffer = new short[readBufferLen];
+
                 while (mActive) {
-                    int ret = mAudioRecord.read(audioData, 0, audioData.length);
+                    int lineMax = 70;
+
+                    int ret = mAudioRecord.read(readBuffer, 0, readBuffer.length);
                     if (ret < 0) {
                         Log.e(TAG, "AudioRecord.read: " + ret);
                         break;
                     }
+                    int nSamples = ret;
 
-                    String audioDataStr = Arrays.toString(audioData);
-                    Log.i(TAG, ". " + audioDataStr.substring(0, Math.min(70, audioDataStr.length())));
-                    nap(1000);
+                    for (int i = 0; i < nSamples; i++) {
+                        double a = (double)readBuffer[i]/Short.MAX_VALUE;
+                        double p = 2 * a * a;
+                        power = alpha * p + (1-alpha) * power;
+                    }
+
+                    Log.i(TAG, String.format("power %.1f", 10*Math.log10(power)));
+                    runOnUiThread(new updateText(power));
                 }
             }
         });
+        mConsumer.start();
     }
+
+    class updateText implements Runnable {
+        double mPower;
+
+        public updateText(double power) {
+            mPower = power;
+        }
+
+        public void run() {
+            view.setText(String.format("power %.1f dB", 10*Math.log10(mPower)));
+        }
+    };
 
     public void onDestroy() {
         mActive = false;
         try {
-            runner.join();
+            if (mConsumer != null)
+                mConsumer.join();
         } catch (Throwable t) {
-            t.printStackTrace();
+            Log.e(TAG, "Thread.join", t);
         }
-        mAudioRecord.stop();
+        if (mAudioRecord != null)
+            mAudioRecord.stop();
 
         super.onDestroy();
     }
@@ -76,7 +132,7 @@ public class Andenv extends Activity
         try {
             Thread.sleep(millis);
         } catch (Throwable t) {
-            t.printStackTrace();
+            Log.e(TAG, "Thread.sleep", t);
         }
     }
 }
